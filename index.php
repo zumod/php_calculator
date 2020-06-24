f287f10d20b4011559213b29e83b09859cf432b7
Sumod Sivadas <contactsumod@gmail.com>
2020-06-24 21:58:28 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+
+$input1 = (int)readline('Enter an integer: '); 
+$input2 = (int)readline('Enter an integer: '); 
+
+echo "The addition of " . $input1 . " and "
+    . $input2 . " is " . ($input1 + $input2); 
+    
+    
+echo "\nThe subtraction of " . $input1 . " and "
+    . $input2 . " is " . ($input1 - $input2); 
+    
+echo "\nThe multiplication of " . $input1 . " and "
+    . $input2 . " is " . ($input1 * $input2); 
+    
+echo "\nThe division of " . $input1 . " and "
+    . $input2 . " is " . ($input1 / $input2);     
+
+?>
